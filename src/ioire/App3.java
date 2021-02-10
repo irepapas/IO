825a9b1d8b4d7f7081f01f91594d26b8f8c7991d
tss <tss@debian>
2021-02-10 17:13:37 +0100

    Prima versione

--- src/ioire/App3.java
+++ src/ioire/App3.java
@@ -5,6 +5,11 @@
  */
 package ioire;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  *
  * @author tss
@@ -12,6 +17,22 @@ package ioire;
 public class App3 {
     public static void main(String[] args) {
         //stampare i nomi dei file testuali (.txt) presenti in una cartella
-        
+        try {
+
+            Path path = FileSystems.getDefault().getPath("");
+            //modo ricorsivo
+            Files.walk(path,1)
+                    .filter(p -> !Files.isDirectory(p))
+                    .filter(p -> p.getFileName().toString().endsWith(".txt"))
+                    .forEach(p -> System.out.println(p.getFileName()));
+            //non ricorsivo
+            Files.list(path)
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .filter(v -> v.endsWith(".txt"))
+                    .forEach(System.out::println);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
